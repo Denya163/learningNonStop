4b34348e72d3f2449866730e3b8b718d44a8f543
Denya163 <denyakiss@yandex.ru>
2025-01-29 15:05:16 +0400

    + myRef

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 
+import myRef from './components/myRef.vue';
+
 </script>
 
 
 
 <template>
-
+  <div>
+    <myRef />
+  </div>
 </template>
 
 
---
+++ src/components/myRef.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0); // Создаем реактивную переменную
+
+const increment = () => {
+  count.value++; // Меняем значение через .value
+};
+</script>
+
+<template>
+  <button @click="increment">Count: {{ count }}</button>
+</template>
