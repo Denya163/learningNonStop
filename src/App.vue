0772b8031d62a4d41df824afa9b2dcff7739a225
Denya163 <denyakiss@yandex.ru>
2025-02-25 15:24:59 +0400

    add router, pinia

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <script setup>
-
-import myRef from '@/components/myRef.vue';
-
+import { useCounterStore } from "@/stores/useCounterStore";
+const counterStore = useCounterStore();
 </script>
 
-
-
 <template>
-  <div>
-    <myRef />
-  </div>
+  <h1>Приветики!</h1>
+  <hr />
+  <button @click="counterStore.increment">+</button>
+  итого: {{ counterStore.count }}
+  <button @click="counterStore.unIncrement">-</button>
 </template>
 
+<style scoped></style>
 
 
-<style scoped>
 
-</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- isPalindrome(value) {
+
+  const checkValue = value.replace(/\s/g, '').toLowerCase();
+  return checkValue === checkValue.split('').reverse().join('')
+
+}; -->
--- src/components/myRef.vue
+++ src/components/myRef.vue
@@ -1,13 +1,50 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 const count = ref(0); // Создаем реактивную переменную
 
 const increment = () => {
   count.value++; // Меняем значение через .value
 };
+
+const inputRef = ref(null);
+
+onMounted(() => {
+  inputRef.value.focus(); // Автоматически ставим фокус на поле ввода
+});
+
+const boxRef = ref(null);
+
+const changeColor = () => {
+  boxRef.value.style.backgroundColor = 'lightblue';
+};
+
+function hiDebounce() {alert('hi')};
+hiDebounce = _.debounce(hiDebounce, 2000);
+
 </script>
 
+
+
 <template>
+  <input ref="inputRef" type="text" placeholder="Введите текст" />
+
   <button @click="increment">Count: {{ count }}</button>
+  <button @click="hiDebounce">Count: {{ count }}</button>
+
+  <div ref="boxRef" class="box">Здесь меняется цвет</div>
+  <button @click="changeColor">Изменить цвет</button>
 </template>
+
+
+
+<style>
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: lightgray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
